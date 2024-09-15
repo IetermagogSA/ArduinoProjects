5706cd44192636c19682aeb4d3dd8d5fe1f1325b
Stefan du Toit <shdutoit@yahoo.com>
2024-09-15 16:49:47 +1000

    Add methods for additional effects with multi-display

--- LEDDisplay/MultiDigit7Segment/MultiDigit7Segment/MultiDigit7Segment.ino
+++ LEDDisplay/MultiDigit7Segment/MultiDigit7Segment/MultiDigit7Segment.ino
@@ -6,29 +6,31 @@ connected to the arduino.
 #include <SevSeg.h>
 
 SevSeg sevSeg;
-byte segmentPins[] = { 5, 4, 2, 3, 6, 7, 8, 9}; // In order A, B, C, D, E, F, G, DP
-byte digitPins[] = {13, 12, 11, 10}; // D4, D3, D2, D1 -- Reading left to right
+int pinA = 5, pinB = 4, pinC = 2, pinD = 3, pinE = 6, pinF = 7, pinG = 8, pinDP = 9; 
+int dp1 = 10, dp2 = 11, dp3 = 12, dp4 = 13;
+byte segmentPins[] = { pinA, pinB, pinC, pinD, pinE, pinF, pinG, pinDP}; // In order A, B, C, D, E, F, G, DP
+byte digitPins[] = {dp4, dp3, dp2, dp1}; // D4, D3, D2, D1 -- Reading left to right
+bool updateWithDelays = false;
 
 
 void setup() {
-  pinMode(5, OUTPUT); // A
-  pinMode(4, OUTPUT); // B
-  pinMode(2, OUTPUT); // C
-  pinMode(3, OUTPUT); // D
-  pinMode(6, OUTPUT); // E
-  pinMode(7, OUTPUT); // F
-  pinMode(8, OUTPUT); // G
-  pinMode(9, OUTPUT); // DP
-  pinMode(10, OUTPUT); // D1
-  pinMode(11, OUTPUT); // D2
-  pinMode(12, OUTPUT); // D3
-  pinMode(13, OUTPUT); // D4
+  pinMode(pinA, OUTPUT); // A
+  pinMode(pinB, OUTPUT); // B
+  pinMode(pinC, OUTPUT); // C
+  pinMode(pinD, OUTPUT); // D
+  pinMode(pinE, OUTPUT); // E
+  pinMode(pinF, OUTPUT); // F
+  pinMode(pinG, OUTPUT); // G
+  pinMode(pinDP, OUTPUT); // DP
+  pinMode(dp1, OUTPUT); // D1
+  pinMode(dp2, OUTPUT); // D2
+  pinMode(dp3, OUTPUT); // D3
+  pinMode(dp4, OUTPUT); // D4
 
   byte numDigits = 4;
 
   bool resistorsOnSegments = true; // We have wired 1 Kohm resistors
   byte hardwareConfig = COMMON_CATHODE;
-  bool updateWithDelays = false;
   bool leadingZeros = false;
   bool disableDecPoint = true;
   sevSeg.setBrightness(90);
@@ -42,7 +44,8 @@ void loop() {
     //display1234();
     //counterLoop();
     //counterLoopReverse();
-    counterLoopDisplay();
+    //counterLoopSegments();
+    counterLoopSegmentsDisplayNum();
 }
 
 void display1234()
@@ -58,7 +61,7 @@ void counterLoop()
   {
     sevSeg.setNumber(i);
     sevSeg.refreshDisplay();
-    //delay(1000);
+    updateWithDelays = true;
   }
 }
 
@@ -68,11 +71,11 @@ void counterLoopReverse()
   {
     sevSeg.setNumber(i);
     sevSeg.refreshDisplay();
-    //delay(1000);
+    updateWithDelays = true;
   }
 }
 
-void counterLoopDisplay()
+void counterLoopSegments()
 {
   for(int d = 0; d < 4; d++)
   {
@@ -85,5 +88,178 @@ void counterLoopDisplay()
     }
 
     sevSeg.refreshDisplay();
+  }
+}
+
+void counterLoopSegmentsDisplayNum()
+{
+  for(int d = 0; d < 4; d++)
+  {
+    clearScreen(false);
+
+    digitalWrite(digitPins[d], LOW);
+
+    // Display a number
+    for(int num = 0; num < 10; num++)
+    {
+      lightSegments();
+      displayNum(num);
+      clearScreen(true);
+      //lightSegments();
+    }
+  }
+
+    clearScreen(false);
+}
+
+void lightSegments()
+{
+  for(int i = 0; i < 8; i++)
+  {
+    digitalWrite(i, HIGH);
+    delay(100);
+  }
+
+  clearScreen(true);
+}
+
+void clearScreen(bool clearSegmentsOnly)
+{
+  digitalWrite(pinA, LOW);
+  digitalWrite(pinB, LOW);
+  digitalWrite(pinC, LOW);
+  digitalWrite(pinD, LOW);
+  digitalWrite(pinE, LOW);
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinG, LOW);
+  digitalWrite(pinDP, LOW);
+
+  if(!clearSegmentsOnly)
+  {
+    digitalWrite(dp1, HIGH);
+    digitalWrite(dp2, HIGH);
+    digitalWrite(dp3, HIGH);
+    digitalWrite(dp4, HIGH);
+  }
+}
+
+void displayNum(int num)
+{
+  switch(num)
+  {
+    case 0:
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, HIGH);
+      digitalWrite(pinE, HIGH);
+      digitalWrite(pinF, HIGH);
+      digitalWrite(pinG, LOW);
+      digitalWrite(pinDP, LOW);
+      delay(1000);
+      break;
+    case 1:
+      digitalWrite(pinA, LOW);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, LOW);
+      digitalWrite(pinE, LOW);
+      digitalWrite(pinF, LOW);
+      digitalWrite(pinG, LOW);
+      digitalWrite(pinDP, LOW);
+      delay(1000);
+      break;
+    case 2:
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, LOW);
+      digitalWrite(pinD, HIGH);
+      digitalWrite(pinE, HIGH);
+      digitalWrite(pinF, LOW);
+      digitalWrite(pinG, HIGH);
+      digitalWrite(pinDP, LOW);
+      delay(1000);
+      break;
+    case 3:
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, HIGH);
+      digitalWrite(pinE, LOW);
+      digitalWrite(pinF, LOW);
+      digitalWrite(pinG, HIGH);
+      digitalWrite(pinDP, LOW);
+      delay(1000);
+      break;
+    case 4:
+      digitalWrite(pinA, LOW);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, LOW);
+      digitalWrite(pinE, LOW);
+      digitalWrite(pinF, HIGH);
+      digitalWrite(pinG, HIGH);
+      digitalWrite(pinDP, LOW);
+      delay(1000);
+      break;
+    case 5:
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, LOW);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, HIGH);
+      digitalWrite(pinE, LOW);
+      digitalWrite(pinF, HIGH);
+      digitalWrite(pinG, HIGH);
+      digitalWrite(pinDP, LOW);
+      delay(1000);
+      break;
+    case 6:
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, LOW);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, HIGH);
+      digitalWrite(pinE, HIGH);
+      digitalWrite(pinF, HIGH);
+      digitalWrite(pinG, HIGH);
+      digitalWrite(pinDP, LOW);
+      delay(1000);
+      break;
+    case 7:
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, LOW);
+      digitalWrite(pinE, LOW);
+      digitalWrite(pinF, LOW);
+      digitalWrite(pinG, LOW);
+      digitalWrite(pinDP, LOW);
+      delay(1000);
+      break;
+    case 8:
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, HIGH);
+      digitalWrite(pinE, HIGH);
+      digitalWrite(pinF, HIGH);
+      digitalWrite(pinG, HIGH);
+      digitalWrite(pinDP, LOW);
+      delay(1000);
+      break;
+    case 9:
+      digitalWrite(pinA, HIGH);
+      digitalWrite(pinB, HIGH);
+      digitalWrite(pinC, HIGH);
+      digitalWrite(pinD, HIGH);
+      digitalWrite(pinE, LOW);
+      digitalWrite(pinF, HIGH);
+      digitalWrite(pinG, HIGH);
+      digitalWrite(pinDP, LOW);
+      delay(1000);
+      break;
+    default:
+      clearScreen(false);
+      delay(1000);
+      break;
   }
 }
